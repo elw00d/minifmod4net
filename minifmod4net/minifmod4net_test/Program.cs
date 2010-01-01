ac23ae86723b1621d1e80efa42a9a983dddffbf0
Kostromin Igor <elw00d@users.noreply.github.com>
2010-01-01 23:32:10 +0000

    *Test program cosmetic changes.

--- minifmod4net/minifmod4net_test/Program.cs
+++ minifmod4net/minifmod4net_test/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Threading;
 using minifmod4net;
 
 namespace minifmod4net_test
@@ -9,26 +10,7 @@ namespace minifmod4net_test
         static void Main(string[] args)
         {
             MiniFmodModuleRegistry registry = new MiniFmodModuleRegistry();
-            using (MiniFmodModule module = registry.LoadFromFile(1, "test.xm")) {
-                module.Play();
-
-                do {
-                    Console.Clear();
-
-                    Console.WriteLine("Order = {0}", module.GetCurrentOrder());
-                    Console.WriteLine("Row = {0}", module.GetCurrentRow());
-                    Console.WriteLine("Time = {0}", module.GetCurrentTime());
-
-
-                    ConsoleKeyInfo info = Console.ReadKey();
-                    if (info.Key == ConsoleKey.Escape) {
-                        break;
-                    }
-                } while (true);
-
-                module.Stop();
-            }
-
+            
             byte[] bytes = File.ReadAllBytes("test.xm");
             using (MiniFmodModule module = registry.LoadFromMemory(1, bytes)) {
                 module.Play();
@@ -41,10 +23,14 @@ namespace minifmod4net_test
                     Console.WriteLine("Time = {0}", module.GetCurrentTime());
 
 
-                    ConsoleKeyInfo info = Console.ReadKey();
-                    if (info.Key == ConsoleKey.Escape) {
-                        break;
+                    if (Console.KeyAvailable) {
+                        ConsoleKeyInfo info = Console.ReadKey();
+                        if (info.Key == ConsoleKey.Escape) {
+                            break;
+                        }
                     }
+
+                    Thread.Sleep(200);
                 } while (true);
 
                 module.Stop();
